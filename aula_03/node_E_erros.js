e9acb1ddae2fe152bfc401ddb874d8ab14fba22a
Bruno <bruno-alura-git@gmail.com>
2022-01-10 20:12:20 -0300

    resumo aula 03

--- aula_03/node_E_erros.js
+++ aula_03/node_E_erros.js
@@ -2,4 +2,8 @@ console.log('\nNesta aula foi comentado sobre o que é o NodeJS\n\nBasicamente c
 
 console.log('\n\nÉ válido lembrar que nossa linguagem de programação é javascript e não NodeJS');
 
-console.log('\n\nNesta aula também foi dada atenção para interpretação de erros comuns que ocorrem no dia a dia com o desenvolvimento de aplicações backend - javascript utilizando NodeJS.')
+console.log('\n\nNesta aula também foi dada atenção para interpretação de erros comuns que ocorrem no dia a dia com o desenvolvimento de aplicações backend - javascript utilizando NodeJS.');
+
+console.log('\n\nÉ válido lembrar que foi comentado do console como um todo. Existem várias aplicações que podem ajudar durante um debug de uma aplicação. Exemplo: temos console.log, temos console.error, como temos muitos outros...');
+
+console.log('\n\nMais informações podem ser encontradas na documentação do NodeJS:\nhttps://nodejs.org/api/console.html');
