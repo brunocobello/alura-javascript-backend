97b3a6532cc0eaace7e8cacb0f4a18c986b1bb75
Bruno <bruno-alura-git@gmail.com>
2022-01-10 20:08:49 -0300

    resumo aula 03

---
+++ aula_03/node_E_erros.js
@@ -0,0 +1,5 @@
+console.log('\nNesta aula foi comentado sobre o que é o NodeJS\n\nBasicamente comentou-se que o NodeJS permite executar o javascript de forma independente do navegador, sendo uma ferramenta ideal para a utilização backend');
+
+console.log('\n\nÉ válido lembrar que nossa linguagem de programação é javascript e não NodeJS');
+
+console.log('\n\nNesta aula também foi dada atenção para interpretação de erros comuns que ocorrem no dia a dia com o desenvolvimento de aplicações backend - javascript utilizando NodeJS.')
